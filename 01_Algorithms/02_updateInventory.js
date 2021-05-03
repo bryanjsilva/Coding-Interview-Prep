60a96e7d8f6d70270a7555822b8929a0ac0ccc2c
bryanjsilva <bryanzsilva@hotmail.com>
2021-05-03 18:07:03 -0500

    Suma de artículos iguales funcionando, utilizando índices

--- 01_Algorithms/02_updateInventory.js
+++ 01_Algorithms/02_updateInventory.js
@@ -1,9 +1,24 @@
 function updateInventory(arr1, arr2) {
     let itemName1 = obtainNames(arr1);
     let itemName2 = obtainNames(arr2);
+    let index1 = [];
+    let index2 = [];
+    
+    for(let element2 of itemName2){
+        for(let element1 of itemName1){
+            if(element2 == element1){
+                index1.push(itemName1.indexOf(element1));
+                index2.push(itemName2.indexOf(element2));
+            }
+        }
+    }
 
-    console.log(itemName1);
-    console.log(itemName2);
+    for(let i = 0; i < index1.length; i ++){
+        arr1[index1[i]][0] += arr2[index2[i]][0];
+    }
+
+    console.log(arr1);
+    console.log(index2);
     return arr1;
 }
 
