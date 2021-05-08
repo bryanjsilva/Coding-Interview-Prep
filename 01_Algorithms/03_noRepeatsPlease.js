bf258a854eaa24bbef1388adf28fa808eea942ca
bryanjsilva <bryanzsilva@hotmail.com>
2021-05-07 20:46:03 -0500

    Se añade la función factorial

--- 01_Algorithms/03_noRepeatsPlease.js
+++ 01_Algorithms/03_noRepeatsPlease.js
@@ -1,5 +1,13 @@
 function permAlone(str) {
-    return str;
-  }
-  
-  permAlone('aab');
+  return str;
+}
+
+function factorial(number){
+    let resultado = 1;
+    for(let i = number; i > 1; i--){
+      resultado *= i;
+    }
+    return resultado;
+}
+
+permAlone('aab');
